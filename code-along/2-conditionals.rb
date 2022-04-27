5a268f043749bbf5eae825754f490b122d24b85a
krystal-o <krystal.onyema@kellogg.northwestern.edu>
2022-04-27 04:45:58 +0000

    created my first craps game

--- code-along/2-conditionals.rb
+++ code-along/2-conditionals.rb
@@ -3,13 +3,49 @@
 # ruby 2-conditionals.rb
 
 # Booleans
+test_is_true = true 
+puts test_is_true
 
+test_is_false = false 
+puts test_is_false
 # Boolean Expressions
+# puts 3 ==2 #returns false
+# puts 3 != 2 #returns true
+# puts 3>2 # returns true
+# puts 3 < 2 # returns false
+# # If Conditional Logic
+# if 3 == 2
+#     puts "This text should never be displayed"
+# end
 
-# If Conditional Logic
+# if 3 > 2
+#     puts "This text should always be displayed"
+# end
 
-# If/Else Conditional Logic
 
-# Elsif Conditional Logic
+# # If/Else Conditional Logic
+# user_entered_password = "tacos"
+# real_password = "secret"
+# if user_entered_password == real_password
+#     puts "Your password matches. You are now logged in!"
+# else
+#     puts "Wrong password. Try again."
+# end
+# # Elsif Conditional Logic
+
+# your_team_score = 2
+# other_team_score = 2
+# if your_team_score > other_team_score
+#     puts "Yay, your team won!"
+# elsif your_team_score == other_team_score
+#     puts "ok, you tied ¯\_(ツ)_/¯"
+# else
+#     puts "Your team lost :("
+# end
 
 # Combining Expressions
+temp = 68
+precipitation = 0
+if temp >= 65 && precipitation == 0 #having issues with adding another temp condition (&& temp =< 75 )
+  puts "It's perfect outside!"
+end
